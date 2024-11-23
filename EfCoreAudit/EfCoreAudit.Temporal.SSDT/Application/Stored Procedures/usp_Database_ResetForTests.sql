c13f0ada54f20669ab7856b24d733b1b42b2c5e8
Philipp Wagner <pwag86@googlemail.com>
2024-11-23 10:24:27 +0100

    Rename Projects

--- EfCoreAudit/EfCoreAudit.Temporal.SSDT/Application/Stored Procedures/usp_Database_ResetForTests.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE PROCEDURE [Application].[usp_Database_ResetForTests]
-AS BEGIN
-    EXECUTE [Application].[usp_TemporalTables_DeactivateTemporalTables];
-
-    EXEC(N'DELETE FROM [Application].[Person]');
-    EXEC(N'DELETE FROM [Application].[PersonHistory]');
-
-    EXECUTE [Application].[usp_TemporalTables_ReactivateTemporalTables];
-END
