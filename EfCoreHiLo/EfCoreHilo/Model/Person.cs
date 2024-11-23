ad2001ea9b320fbec41c7e197c3d4745192d2fef
Philipp Wagner <pwag86@googlemail.com>
2024-11-23 10:27:59 +0100

    Rename Projects

---
+++ EfCoreHiLo/EfCoreHilo/Model/Person.cs
@@ -0,0 +1,30 @@
+﻿// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace EfCoreAudit.Temporal.Model
+{
+    /// <summary>
+    /// A Person in the application.
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// Gets or sets the Id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Name.
+        /// </summary>
+        public required string FullName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ValidFrom.
+        /// </summary>
+        public DateTime? ValidFrom { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ValidTo
+        /// </summary>
+        public DateTime? ValidTo { get; set; }
+    }
+}
